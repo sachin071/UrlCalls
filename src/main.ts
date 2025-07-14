dff50c2afc5b169e86eaec01ab6d895f7be72abe
sachin071 <indegamer2@gmail.com>
2025-07-14 22:43:40 +0530

    Finalizing

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as os from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: '*', // Specify your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Include credentials if needed
+  });
+
   await app.listen(process.env.PORT ?? 3000 , '0.0.0.0');
 }
 bootstrap();
