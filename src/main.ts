c6f5b9a0e377ca71ec4895ae36fe6f8362da08c4
sachin071 <indegamer2@gmail.com>
2025-07-14 22:53:47 +0530

    Finally Should Work

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../src/app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import { AppModule } from './app.module';
 
-const server = express();
-
-const createNestApp = async () => {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
-    origin: '*',
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    credentials: true,
+    origin: '*', // Specify your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Include credentials if needed
   });
 
-  await app.init();
-};
-
-createNestApp();
-
-export default server;
+  await app.listen(process.env.PORT ?? 3000 , '0.0.0.0');
+}
+bootstrap();
