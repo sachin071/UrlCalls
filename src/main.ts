fd83d962a45873ceac1de46ae1004a7c2c9252ee
sachin071 <indegamer2@gmail.com>
2025-07-14 22:48:29 +0530

    Final

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+const server = express();
+
+const createNestApp = async () => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   app.enableCors({
-    origin: '*', // Specify your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Include credentials if needed
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000 , '0.0.0.0');
-}
-bootstrap();
+  await app.init();
+};
+
+createNestApp();
+
+export default server;
